f7daca8fa71c2c6845dd0db699071120bdf38139
Syuro <64142347+syurodev@users.noreply.github.com>
2022-12-01 18:45:10 +0700

    Fix CORS

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const app = require("express")();
 const http = require("http").Server(app);
-const io = require("socket.io")(http);
+const io = require("socket.io")(http, {
+	cors: {
+		origin: "*"
+	}
+});
 const mongoose = require("mongoose");
 let users = [];
 let messages = [];
